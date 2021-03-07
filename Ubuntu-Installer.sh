58ce33b87efcec2548dad6ea82b00fd91abb7dc9
time-killer-games <samuelvenable@hotmail.com>
2021-03-07 15:36:33 -0500

    Update Ubuntu-Installer.sh

--- Ubuntu-Installer.sh
+++ Ubuntu-Installer.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 cd ~/
 echo "Installing dependencies..."
-sudo add-apt-repository ppa:maarten-fonville/protobuf
-sudo apt-get update
 sudo apt-get install wget git g++ make build-essential libprotobuf-dev protobuf-compiler zlib1g-dev libglew-dev libglm-dev libpng-dev libglu1-mesa-dev libopenal-dev libogg-dev libalure-dev libvorbisfile3 libvorbis-dev libbox2d-dev libdumb1-dev libsdl2-dev libfreetype6-dev libffi-dev libx11-dev libxrandr-dev libxinerama-dev libprocps-dev libepoxy-dev default-jre default-jdk pkg-config rapidjson-dev libyaml-cpp-dev libboost-dev libboost-filesystem-dev libboost-system-dev libboost-program-options-dev libboost-iostreams-dev pulseaudio libpugixml-dev zenity kdialog mpv
 echo "Downloading Enigma..."
 git clone git://github.com/time-killer-games/enigma-dev-fork.git ~/enigma-dev
